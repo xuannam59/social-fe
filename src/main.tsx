e86e4064097efc799df67c94ae3f710b974d6864
xuannam59 <leeminhnam2k2@gmail.com>
2025-12-01 22:12:26 +0700

    Use StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import { store } from '@social/redux/stores.ts';
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <Provider store={store}>
-    {/* <StrictMode> */}
-    <App />
-    {/* </StrictMode> */}
+    <StrictMode>
+      <App />
+    </StrictMode>
   </Provider>
 );
